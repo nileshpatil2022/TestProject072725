ba88c11f74de5e61c3b857cd8cf710f14f321b81
Nilesh Patil <nileshbpatil152@gmail.com>
2025-11-26 23:32:12 +0530

    new package and class

---
+++ src/test/java/newPack/Demo1.java
@@ -0,0 +1,15 @@
+package newPack;
+
+public class Demo1 
+{
+	public static void main(String[] args) {
+		System.out.println("working directory");
+		
+		System.out.println("staging area");
+		
+		System.out.println("Git local repository");
+		
+		System.out.println("GitHub remote repository");
+	}
+
+}
